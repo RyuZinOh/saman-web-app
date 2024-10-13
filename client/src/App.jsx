b04259b1b833a3749a1003038fdfbfbcada3fd3d
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-10-13 21:41:36 +0545

    c1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
     </BrowserRouter>
   );
