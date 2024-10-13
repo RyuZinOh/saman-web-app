98b5b44451520f0ab667ad82be9cf2d0c38d22a9
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-10-13 23:18:36 +0545

    b / ?

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this matches your deployment settings
+    outDir: "dist",
   },
+  base: "/", // Set the base path, usually '/' for root but can be a subdirectory if needed
 });
